0e5a9454ef511863aa979c08aa87aab959e91841
Vanderlei Bandeira <vanderlei123bandeira4@gmail.com>
2025-09-17 21:52:15 -0300

    exercicios de objetos

---
+++ exercicios-objetos/.js/exercicio-lista-objetos.js
@@ -0,0 +1,99 @@
+function listaDeCarros() {
+    let carros = [
+        {
+            marca: "Chevrolet",
+            modelo: "Onix",
+            placa: "BRA2E19",
+            cor: "Azul",
+            data_emplacamento: 2022
+        },
+        {
+            marca: "Fiat",
+            modelo: "Uno",
+            placa: "BRA2B16",
+            cor: "Verde",
+            data_emplacamento: 2020
+        },
+        {
+            marca: "Citroen",
+            modelo: "C3",
+            placa: "BRA5F15",
+            cor: "Branco",
+            data_emplacamento: 2023
+        },
+        {
+            marca: "kIA",
+            modelo: "Picanto",
+            placa: "BRA2R23",
+            cor: "Laranja",
+            data_emplacamento: 2019
+        },
+        {
+            marca: "Renault",
+            modelo: "Kwid",
+            placa: "BRA2H15",
+            cor: "preto",
+            data_emplacamento: 2024
+        },
+        {
+            marca: "BMW",
+            modelo: "Bmw x4",
+            placa: "BRA2W21",
+            cor:"Cinza",
+            data_emplacamento: 2025
+        },
+        {
+            marca: "JEEP",
+            modelo: "Renegade",
+            placa: "BRA2l13",
+            cor:"Vermelho",
+            data_emplacamento: 2022
+        }
+
+    ]
+    
+     
+    let marcas = carros.map(carro => {
+        return {
+            marca: carro.marca,
+            modelo: carro.modelo
+        };    
+    });
+
+    console.log(marcas)
+   
+    let carroFiat = carros.filter(carro => carro.marca == "Fiat").map(carro => carro.modelo);
+    
+    console.log(carroFiat)
+
+    let carroCor = carros.filter( carro =>  carro.cor == "Azul").map(carro => {
+        return {
+            cor: carro.cor,
+            modelo: carro.modelo
+         
+        };
+    });
+
+    console.log(carroCor)
+
+  let ano = carros.filter(carro => {
+    let anoplaca = new Date(carro.data_emplacamento);
+    if (anoplaca.getFullYear() === 2025) {
+        return true;
+    } else {
+        return false;
+    }
+  });
+
+  console.log(ano)
+   
+}
+ 
+
+
+
+
+
+
+
+
---
+++ exercicios-objetos/.js/js/listaAlunos.js
@@ -0,0 +1,37 @@
+let listaAlunos = [
+    {
+        "id": crypto.randomUUID(),
+        "nome": "Felipe",
+        "sobrenome": "fernando",
+        "numeromatricula": 4678910,
+        "turma": "sup 07",
+         "notas": [7.5,10,4.6]
+    },
+    {
+        "id": crypto.randomUUID(),
+        "nome": "Vanderlei",
+        "sobrenome": "Bandeira",
+        "numeromatricula": 4678945,
+        "turma": "sup 06",
+         "notas": [7.5,10,4.6]
+    }
+
+]
+
+let nome = "Vanderlei";
+let sobrenome = "Bandeira";
+let notas = []
+
+listaAlunos.push({
+    "id": crypto.randomUUID(),
+    "nome": nome,
+    "sobrenome": sobrenome,
+    "notas": notas
+});
+
+
+
+
+
+
+
